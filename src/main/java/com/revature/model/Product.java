27528e1fb92079e199f94e75f32d1939f8eadbc6
Joel De Jesus <Joel@Joels-MacBook-Pro.local>
2018-04-05 13:19:09 -0400

    Version#2 of the product pojo, nowith mapped by! also ajusted the name field to productName for clarity

--- src/main/java/com/revature/model/Product.java
+++ src/main/java/com/revature/model/Product.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -23,23 +23,29 @@ public class Product {
 	private String description;
 	
 	@Column(name = "P_NAME")
-	private String name;
+	private String productName;
 	
 	@Column(name = "P_IMAGE_URL")
 	private String image;
 	
-	@Column(name = "P_PRODUCT_SELLING")
-	private long productSelling;
+	@OneToMany(mappedBy= "EWS_PURCHASE")
+	private long sId;
+	
+	@OneToMany(mappedBy = "EWS_SELLING")
+	private long pcId;
 	
 	public Product() {}
 	
-	public Product(long id, String description, String image, long productSelling) {
+	public Product(long id, String description, String productName, String image, long sId, long pcId) {
+		super();
 		this.id = id;
 		this.description = description;
-		this.image=image;
-		this.productSelling = productSelling;
+		this.productName = productName;
+		this.image = image;
+		this.sId = sId;
+		this.pcId = pcId;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
@@ -52,18 +58,34 @@ public class Product {
 		return description;
 	}
 	
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public long getsId() {
+		return sId;
+	}
+
+	public void setsId(long sId) {
+		this.sId = sId;
+	}
+
+	public long getPcId() {
+		return pcId;
+	}
+
+	public void setPcId(long pcId) {
+		this.pcId = pcId;
+	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
 	
-	public long getProductSelling() {
-		return productSelling;
-	}
-
-	public void setProductSelling(long productSelling) {
-		this.productSelling = productSelling;
-	}
-
 	public String getImage() {
 		return image;
 	}
@@ -72,9 +94,5 @@ public class Product {
 		this.image = image;
 	}
 	
-	@Override
-	public String toString() {
-		return "Product [id=" + id + ", description=" + description + ", image=" + image + ", productSelling=" + productSelling + "]";
-	}
 	
 }
