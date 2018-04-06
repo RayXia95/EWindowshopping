5ccc4fb4f4f2494b34fef39462d192dfca9bf635
Joel De Jesus <Joel@Joels-MBP.home>
2018-04-05 20:12:21 -0400

    added finalized mapped by

--- src/main/java/com/revature/model/Product.java
+++ src/main/java/com/revature/model/Product.java
@@ -12,52 +12,70 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "EWS_PRODUCT")
 public class Product {
-	
+
 	@Id
 	@Column(name = "P_ID")
 	@GeneratedValue(strategy =GenerationType.SEQUENCE,generator = "productSequence")
 	@SequenceGenerator(name ="productSequence", sequenceName="PRODUCT_SEQ", allocationSize=1)
 	private long id;
-	
+
 	@Column(name = "P_DESCRIPTION")
 	private String description;
-	
+
 	@Column(name = "P_NAME")
 	private String productName;
-	
+
 	@Column(name = "P_IMAGE_URL")
 	private String image;
-	
+
 	@OneToMany(mappedBy= "EWS_PURCHASE")
-	private long sId;
-	
+	private Purchase purchase;
+
 	@OneToMany(mappedBy = "EWS_SELLING")
-	private long pcId;
-	
+	private Selling selling;
+
 	public Product() {}
-	
-	public Product(long id, String description, String productName, String image, long sId, long pcId) {
-		super();
+
+	public Product(long id, String description, String productName, String image, Purchase purchase, Selling selling) {
 		this.id = id;
 		this.description = description;
 		this.productName = productName;
 		this.image = image;
-		this.sId = sId;
-		this.pcId = pcId;
+		this.purchase = purchase;
+		this.selling = selling;
 	}
+
+
+	public Purchase getPurchase() {
+		return purchase;
+	}
+
+	public void setPurchase(Purchase purchase) {
+		this.purchase = purchase;
+	}
+
+	public Selling getSelling() {
+		return selling;
+	}
+
+	public void setSelling(Selling selling) {
+		this.selling = selling;
+	}
+
+
 
 	public long getId() {
 		return id;
 	}
-	
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public String getProductName() {
 		return productName;
 	}
@@ -66,30 +84,16 @@ public class Product {
 		this.productName = productName;
 	}
 
-	public long getsId() {
-		return sId;
-	}
 
-	public void setsId(long sId) {
-		this.sId = sId;
-	}
-
-	public long getPcId() {
-		return pcId;
-	}
-
-	public void setPcId(long pcId) {
-		this.pcId = pcId;
-	}
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
 	public String getImage() {
 		return image;
 	}
-	
+
 	public void setImage(String image) {
 		this.image = image;
 	}
@@ -97,9 +101,11 @@ public class Product {
 	@Override
 	public String toString() {
 		return "Product [id=" + id + ", description=" + description + ", productName=" + productName + ", image="
-				+ image + ", sId=" + sId + ", pcId=" + pcId + "]";
+				+ image + ", purchase=" + purchase + ", selling=" + selling + "]";
 	}
-	
-	
-	
+
+
+
+
+
 }
