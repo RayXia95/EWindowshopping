322da2d2a2408fb8a434d67186fb5e6b17cd2a9a
Mark Fleres <markfleres@gmail.com>
2018-04-06 12:49:07 -0400

    Added ClientMessage and package com.revature.rest (along with adding it to the context)

--- src/main/java/com/revature/service/UserServiceAlpha.java
+++ src/main/java/com/revature/service/UserServiceAlpha.java
@@ -1,10 +1,12 @@
 package com.revature.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.revature.model.User;
 import com.revature.repository.UserRepository;
 
+@Service("userService")
 public class UserServiceAlpha implements UserService {
 
 	@Autowired
