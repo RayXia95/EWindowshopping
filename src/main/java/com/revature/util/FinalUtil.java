d23c78c407cfd8d012ef5ce0d9fad100db118346
Joel De Jesus <Joel@Joels-MacBook-Pro.local>
2018-04-09 13:02:06 -0400

    Forgot to include util class

---
+++ src/main/java/com/revature/util/FinalUtil.java
@@ -0,0 +1,11 @@
+package com.revature.util;
+
+import com.revature.ajax.ClientMessage;
+
+public final class FinalUtil {
+	public static final ClientMessage PURCHASE_SUCCESSFUL= new ClientMessage("PURCHASE SUCCESSFUL");
+	public static final ClientMessage SELL_SUCCESSFUL= new ClientMessage("SELLING SUCCESSFUL");
+	public static final ClientMessage UPDATE_SUCCESSFUL= new ClientMessage("UPDATE SUCCESSFUL");
+	public static final ClientMessage SOMETHING_WRONG= new ClientMessage("SOMETHING WENT WRONG");
+
+}
