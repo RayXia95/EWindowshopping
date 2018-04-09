980ee0968abf0043476acdb0a0f8e5e2f1743aa8
Mark Fleres <markfleres@gmail.com>
2018-04-09 19:37:29 -0400

    Merged FinalUtil into ClientMessageUtil

--- EWSJava/src/main/java/com/revature/util/ClientMessageUtil.java
+++ EWSJava/src/main/java/com/revature/util/ClientMessageUtil.java
@@ -6,4 +6,7 @@ public final class ClientMessageUtil {
 	public static final ClientMessage REGISTRATION_SUCCESSFUL = new ClientMessage("REGISTRATION SUCCESSFUL");
 	public static final ClientMessage SOMETHING_WRONG = new ClientMessage("SOMETHING WENT WRONG");
 	public static final ClientMessage INVALID_LOGIN = new ClientMessage("INVALID LOGIN");
+	public static final ClientMessage PURCHASE_SUCCESSFUL= new ClientMessage("PURCHASE SUCCESSFUL");
+	public static final ClientMessage SELL_SUCCESSFUL= new ClientMessage("SELLING SUCCESSFUL");
+	public static final ClientMessage UPDATE_SUCCESSFUL= new ClientMessage("UPDATE SUCCESSFUL");
 }
