b875c09c4c3dfaa5b331372111af5bdba130b939
Mark Fleres <markfleres@gmail.com>
2018-04-11 12:03:01 -0400

    Added client-message model; added EWS_URL env variable; added login and handleError to LoginService

--- ews-angular/src/app/services/login.service.ts
+++ ews-angular/src/app/services/login.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
 
+import { Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs/Observable";
+import 'rxjs/Rx';
+
+import { User } from '../models/user.model';
+import { ClientMessage } from '../models/client-message.model';
+import { EWS_URL } from '../../environments/environment';
+
 @Injectable()
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  public login(user: User) : Observable<Object> {
+    return this.http
+                .post(`${EWS_URL}login`,user)
+                .catch(this.handleError);
+  }
+
+  public handleError(error: Response) {
+    return Observable.throw(error.statusText);
+  }
 }
