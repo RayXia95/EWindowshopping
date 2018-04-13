d79e0c31928f00f2aa569821be8f2e3cf1f364d6
Alex I <aiparrag3@gmail.com>
2018-04-13 11:35:44 -0400

    Angular All

--- EWSJava/src/main/java/com/revature/rest/ProductControllerAlpha.java
+++ EWSJava/src/main/java/com/revature/rest/ProductControllerAlpha.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,11 +12,12 @@ import com.revature.model.Selling;
 import com.revature.service.ProductService;
 
 @Controller("productController")
+@CrossOrigin(origins={"http://localhost:4200"})
 public class ProductControllerAlpha implements ProductController {
 	@Autowired
 	private ProductService productService;
 	
-	@GetMapping("/getAllProducts")
+	@GetMapping(value="/getAllProducts")
 	public @ResponseBody List<Selling> findAllProducts() {
 		return productService.getAllProducts();
 	}
