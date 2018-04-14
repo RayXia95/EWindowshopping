d7e5d3471a489c1683636707046c1c702f3f543e
Mark Fleres <markfleres@gmail.com>
2018-04-14 12:34:39 -0400

    Added the business logic of making a purchase to PurchaseService

--- EWSJava/src/main/java/com/revature/service/PurchaseServiceAlpha.java
+++ EWSJava/src/main/java/com/revature/service/PurchaseServiceAlpha.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.revature.model.Purchase;
+import com.revature.model.Selling;
+import com.revature.model.User;
 import com.revature.repository.PurchaseRepository;
 @Repository("purchaseService")
 public class PurchaseServiceAlpha implements PurchaseService{
@@ -15,8 +17,27 @@ public class PurchaseServiceAlpha implements PurchaseService{
 
 	@Override
 	public boolean purchaseProduct(Purchase purchase) {
+		// Unpacking some variables from purchase
+		User seller = purchase.getSelling().getSeller();
+		User buyer = purchase.getBuyer();
+		Selling selling = purchase.getSelling();
+
+		if (buyer.getBalance() < selling.getCost()) {
+			// To expensive for buyer
+			return false;
+		} else if (purchase.getQuantity() > selling.getQuantity()) {
+			// Not enough in stock
+			return false;
+		} else {
+			double balanceChange = purchase.getQuantity() * selling.getCost();
+			buyer.setBalance(buyer.getBalance() - balanceChange);
+			seller.setBalance(seller.getBalance() + balanceChange);
+
+			selling.setQuantity(selling.getQuantity() - purchase.getQuantity());
+
 			purchaseRepository.save(purchase);
-			return purchase.getBuyer().getId()!=0;
+			return purchase.getBuyer().getId() != 0;
+		}	
 	}
 
 	@Override
